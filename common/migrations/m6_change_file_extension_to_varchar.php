38eff48fb8081ff670ca78b2d20c451001080ba8
Martin Thrömer <m.throemer@gmail.com>
2022-05-14 18:20:50 +0200

    Update m6_change_file_extension_to_varchar.php

--- common/migrations/m6_change_file_extension_to_varchar.php
+++ common/migrations/m6_change_file_extension_to_varchar.php
@@ -12,7 +12,7 @@ class m6_change_file_extension_to_varchar
      */
     public static function up()
     {
-        DBModel::execute()->sql( "ALTER TABLE `queue_ocr_api` CHANGE `file_extension` `file_extension` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;" );
+        DBModel::execute()->sql( "ALTER TABLE `queue_ocr` CHANGE `file_extension` `file_extension` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;" );
     }
 
     /**
@@ -20,6 +20,6 @@ class m6_change_file_extension_to_varchar
      */
     public static function down()
     {
-        DBModel::execute()->sql( "ALTER TABLE `queue_ocr_api` CHANGE `file_extension` `file_extension` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;" );
+        DBModel::execute()->sql( "ALTER TABLE `queue_ocr` CHANGE `file_extension` `file_extension` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;" );
     }
 }
