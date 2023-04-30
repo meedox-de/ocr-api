5f6e039d3afb5079e7c0ee6ba3a03562fa8e47e5
Martin Thrömer <m.throemer@gmail.com>
2023-04-30 02:37:16 +0200

    Update routes.php

--- console/config/routes.php
+++ console/config/routes.php
@@ -4,7 +4,7 @@
  * token => controller,
  */
 $routes = [
-    '07ducf81' => 'migration',
+    '07ducf81' => 'Migration',
     'zh8iz9ja' => 'ApiDataExchange',
     '48fk4k5p' => 'CronjobOcrRequest',
     'kk4k56j7' => 'TikaController',
